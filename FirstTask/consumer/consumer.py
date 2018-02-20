4050fa5ecbdb2d5b074ea2f370a0d62d368a3788
Ivan Provilkov <panzer310@yandex.ru>
2018-02-20 11:38:44 +0300

    Delete some bags

--- FirstTask/consumer/consumer.py
+++ FirstTask/consumer/consumer.py
@@ -21,12 +21,9 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-
 def callback(ch, method, properties, body):
     log.info("Message received: {}".format(body))
-    with open("testfile.txt", "w") as f:
-        f.write(" [x] Received %r" % body)
-    post = str(body)
+    post = {str(body): str(body)}
     posts.insert_one(post)
 
 
